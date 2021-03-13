bf19459928f9c60fc1a906debb324dd35734d473
KevinChia <KevinChia@github.com>
2021-03-13 23:36:50 +0800

    message

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,9 @@ docker push s0261105/multi-worker:$SHA
 docker push s0261105/multi-client:$SHA
 docker push s0261105/multi-server:$SHA
 
+
+kubeclt apply -f ./k8s
+
 kubectl set image deployments/server-deployment server=s0261105/multi-server:$SHA
 kubectl set image deployments/client-deployment client=s0261105/multi-client:$SHA
 kubectl set image deployments/worker-deployment worker=s0261105/multi-worker:$SHA
